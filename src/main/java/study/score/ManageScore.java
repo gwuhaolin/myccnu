6f87d8a5a41c0b33512cbc285724d8ac804f18dc
wuhaolin <gwuhaolin@gmail.com>
2014-07-06 15:35:01 +0800

    重新写了异常

--- src/main/java/study/score/ManageScore.java
+++ src/main/java/study/score/ManageScore.java
@@ -15,15 +15,11 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import tool.CCNUJWC;
-import tool.HibernateUtil;
-import tool.R;
+import tool.*;
+import tool.studentInfo.StudentInfoEntity;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 从教务处网站获得成绩
@@ -40,29 +36,29 @@ public class ManageScore {
 	/**
 	 * 查询成绩,获得这学期的最新的成绩结果
 	 * 查询结果按照总分大小排序
+	 * 结果会更新的数据库
 	 *
 	 * @param XH 学号
 	 * @param MM 密码
-	 * @return
-	 * 如果网络错误或学校服务器错误就从本机的数据库里获得
+	 * @return 如果网络错误或学校服务器错误就从本机的数据库里获得
 	 */
 	public static List<MyScoreEntity> get(String XH, String MM) {
-		Map<String, String> cookies = null;
+		Map<String, String> cookies;
 		try {
 			cookies = CCNUJWC.getCookie(XH, MM);
-		} catch (Exception e) {
-			log.error(e.getMessage());
+		} catch (NetworkException | ValidateException e) {
+			log.error(Arrays.toString(e.getStackTrace()));
 			return get_XH(XH);
 		}
 		Connection connection = Jsoup.connect(URL_CJ);
 		connection.userAgent(R.USER_AGENT);
 		connection.timeout(R.ConnectTimeout);
 		connection.cookies(cookies);
-		Document document = null;
+		Document document;
 		try {
 			document = connection.post();
 		} catch (IOException e) {
-			log.error(e.getMessage());
+			log.error(Arrays.toString(e.getStackTrace()));
 			return get_XH(XH);
 		}
 		try {
@@ -76,6 +72,8 @@ public class ManageScore {
 			connection.data("__EVENTVALIDATION", __EVENTVALIDATION);
 			connection.data("Button1", "查询");
 		} catch (IndexOutOfBoundsException e) {
+			log.error(Arrays.toString(e.getStackTrace()));
+
 			log.error(document.toString());
 			return get_XH(XH);
 		}
@@ -84,7 +82,8 @@ public class ManageScore {
 		try {
 			document = connection.post();
 		} catch (IOException e) {
-			log.error(e.getMessage());
+			log.error(Arrays.toString(e.getStackTrace()));
+			log.error(document.toString());
 			return get_XH(XH);
 		}
 		//解析数据
@@ -98,8 +97,10 @@ public class ManageScore {
 			}
 			saveOrUpdateScores(re);//保存到数据库
 			Collections.sort(re);
+			log.info("学号为{}的查询成绩成功", XH);
 			return re;
 		} catch (IndexOutOfBoundsException e) {
+			log.error(Arrays.toString(e.getStackTrace()));
 			log.error(document.toString());
 			return get_XH(XH);
 		}
@@ -116,6 +117,12 @@ public class ManageScore {
 		}
 	}
 
+	/**
+	 * 获得所有学号等于xh的课程
+	 *
+	 * @param xh 学生的学号
+	 * @return 课程
+	 */
 	public static List<MyScoreEntity> get_XH(String xh) {
 		Session session = HibernateUtil.getSession();
 		Query query = session.createQuery("from MyScoreEntity where xh=?");
@@ -126,6 +133,12 @@ public class ManageScore {
 		return re;
 	}
 
+	/**
+	 * 获得所有的classID为classID的课程
+	 *
+	 * @param classNo 课程的id
+	 * @return 所有的课程
+	 */
 	public static List<MyScoreEntity> get_ClassNo(String classNo) {
 		Session session = HibernateUtil.getSession();
 		Query query = session.createQuery("from MyScoreEntity where classNo=?");
@@ -134,6 +147,22 @@ public class ManageScore {
 		HibernateUtil.closeSession(session);
 		Collections.sort(re);
 		return re;
+	}
+
+	/**
+	 * 对于数据库中所有已经知道了帐号密码的同学,主动去教务处抓取数据
+	 *
+	 * @return 更新了的人数
+	 */
+	public static int updateAllStudentsScore() {
+		Session session = HibernateUtil.getSession();
+		Query query = session.createQuery("from StudentInfoEntity where password!=null and xh!=null order by xh desc ");
+		List<StudentInfoEntity> studentInfoEntities = query.list();
+		HibernateUtil.closeSession(session);
+		for (StudentInfoEntity studentInfoEntity : studentInfoEntities) {
+			get(studentInfoEntity.getXh(), studentInfoEntity.getPassword());
+		}
+		return studentInfoEntities.size();
 	}
 
 }
