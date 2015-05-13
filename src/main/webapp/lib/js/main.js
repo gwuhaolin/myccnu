10aa5c383201a16b389db421f8af485ab4cdc8b4
WuHaolin <gwuhaolin@gmail.com>
2015-05-13 21:40:14 +0800

    添加书循广告

--- src/main/webapp/lib/js/main.js
+++ src/main/webapp/lib/js/main.js
@@ -167,7 +167,7 @@ function getCookie(c_name) {
 //显示广告
 function showAD() {
     var adHtml = $('<a class="sixteen wide column center aligned" href="http://mp.weixin.qq.com/s?__biz=MzAwMDQwMjMxNg==&mid=205566574&idx=1&sn=5784b3b5f4d870ca4715c2dd56d8f01e#rd"><img src="/AD.png" style="width: 100%;height: auto"></a>');
-    $(document.body).append(adHtml);
+    $(document.body).prepend(adHtml);
     setTimeout(function () {
         $(adHtml).remove();
     }, 5000);
